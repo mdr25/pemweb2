7eb60b08b994873c840885cb9f18a55bd951d188
mdr25 <dihyaspider20@gmail.com>
2023-05-25 20:24:15 +0700

    week10 laravel - halaman About

---
+++ week10/resources/views/toko/about.blade.php
@@ -0,0 +1,56 @@
+@extends('template.main')
+
+@section('content')
+    <div class="row my-5">
+        <div class="col-md-6">
+            <img src="/img/img4.jpg" alt="" class="shadow-lg w-100 rounded" />
+        </div>
+        <div class="col-md-6">
+            <h1 class="display-4 fw-bold">Welcome to P SIAP!</h1>
+            <p class="me-3">
+                We are a company dedicated to providing quality mountaineering equipment for nature
+                enthusiasts and mountain climbers worldwide. With our experience and expertise, we are committed to ensuring
+                you have the right and reliable gear for your outdoor adventures.
+
+                We believe that nature is an incredible place that offers unforgettable experiences. Therefore, we want to
+                ensure that adventurers have access to equipment that meets their needs, allowing them to enjoy and maintain
+                safety while exploring mountains.
+
+                Quality is one of the core principles of our philosophy. We only provide the best products from leading
+                brands that have proven to be reliable and durable in challenging mountain terrains. Our team conducts
+                thorough research to select products that meet high safety and comfort standards.
+
+                Moreover, we recognize that every adventure is unique, and each individual has different needs. Therefore,
+                we offer a variety of mountaineering equipment options to meet the diverse requirements of our customers.
+                From tents, sleeping bags, hiking boots, to other climbing gear, we have a wide range of products to fulfill
+                your adventure needs.
+            </p>
+        </div>
+    </div>
+@endsection
+
+@section('content2')
+    <div class="row my-5">
+        <div class="col-md-6">
+            <p class="me-3 my-5">
+                Our commitment to customers does not end with the transaction. We are dedicated to providing superior
+                customer service. Our experienced and friendly team is ready to assist you in making the right choices,
+                providing expert advice, and answering all your questions about mountaineering equipment.
+
+                We are also committed to promoting environmental awareness and preserving nature. We collaborate with
+                brands that care about the environment and prioritize sustainable business practices. By purchasing
+                products from us, you are also contributing to environmental conservation.
+
+                We value the trust and support of our customers. With this spirit, we strive to continuously improve our
+                services and ensure customer satisfaction at all times.
+
+                Thank you for visiting P SIAP. We hope to become your trusted mountaineering equipment partner and
+                accompany you on every unforgettable outdoor adventure. If you have any further questions or need
+                assistance, please feel free to contact our team. Happy mountain exploration!
+            </p>
+        </div>
+        <div class="col-md-6">
+            <img src="/img/img5.jpg" alt="" class="shadow-lg w-100 rounded" />
+        </div>
+    </div>
+@endsection
